af13998c2be5b2fc5f9f1cdbf557ecf3e939efd9
Stephen Sheldon <20730278+ssheld@users.noreply.github.com>
2022-04-03 18:35:12 -0400

    remove unnecessary router code

--- src/index.js
+++ src/index.js
@@ -4,17 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 ReactDOM.render(
   <AuthContextProvider>
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact>
           <App />
-        </Route>
-      </Switch>
-    </BrowserRouter>
   </AuthContextProvider>,
   document.getElementById("root")
 );
