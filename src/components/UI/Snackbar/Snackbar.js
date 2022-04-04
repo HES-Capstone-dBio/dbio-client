53f2985d65c6a660fa1206ffb88677e34cd1008c
Stephen Sheldon <20730278+ssheld@users.noreply.github.com>
2022-04-04 09:10:46 -0400

    Create snackbar to display success/error messages

---
+++ src/components/UI/Snackbar/Snackbar.js
@@ -0,0 +1,62 @@
+import classes from "./Snackbar.module.css";
+
+let previous;
+
+/**
+ * Clear the snackbar either by removing it completely, or by transitioning it out by setting the
+ * opacity/top rules correctly.
+ */
+function dismissSnackbar(element, force) {
+    if (element) {
+        if (force) {
+            document.body.removeChild(element);
+        } else {
+            element.style.opacity = "0";
+            element.style.top = "0";
+        }
+    }
+}
+
+function dismissAfterTimeout(item) {
+    if (item) {
+        dismissSnackbar(item, false);
+    }
+}
+
+/**
+ * Displays a "snackbar" UI to show the user a success/info/fail message. Message will appear
+ * in top center of screen and disappear after a set amount of time
+ * @param message Label to display in snackbar
+ * @param type    Type of message to display. Either success/info/error/warning
+ * @param timeout Length in ms that snackbar will appear
+ */
+const snackbar = (message, type = "success") => {
+    const timeout = type === "error" ? 4000 : 2000;
+    dismissSnackbar(previous, true);
+    const snackbar = document.createElement("div");
+    snackbar.className = `${classes.snackbar} ${classes[`snackbar-${type.charAt(0).toUpperCase() + type.slice(1)}`]}`;
+    snackbar.setAttribute("role", "alert");
+    //Adjust top position to account for the users scroll height
+    snackbar.style.top = `${window.scrollY}px`;
+
+    snackbar.appendChild(document.createTextNode(message));
+
+    setTimeout(dismissAfterTimeout.bind(null, snackbar), timeout);
+    snackbar.addEventListener("transitionend", () => {
+        //Check if both transitions are complete before removing the element
+        if (previous && previous.style.opacity === "0" && previous.style.top === "0px") {
+            dismissSnackbar(previous, true);
+            previous = null;
+        }
+    });
+
+    previous = snackbar;
+    document.body.appendChild(snackbar);
+    /* eslint-disable */
+    getComputedStyle(snackbar).top;
+    /* eslint-disable */
+    snackbar.style.top = `${document.getElementById("dcp-gs-chunk").offsetTop - 19}px`;
+    snackbar.style.opacity = "1";
+};
+
+export default snackbar;
