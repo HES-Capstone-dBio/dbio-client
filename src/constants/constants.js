93bd4da0846720dde2c5ea561dbbe5b7ab7f05d5
Stephen Sheldon <20730278+ssheld@users.noreply.github.com>
2022-04-27 22:05:31 -0400

    Remove unused constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,12 +2,4 @@ export const GOOGLE = "google";
 export const AUTH0_DOMAIN = "https://dbio.us.auth0.com";
 export const AUTH0_CLIENT_ID = "xIEcOQHJ1jJYClYvPS2EFAcinK0w7R2K";
 export const BACKEND_ENDPOINT = "http://localhost:8080";
-
-export const verifierMap = {
-  [GOOGLE]: {
-    name: "Google",
-    typeOfLogin: "jwt",
-    verifier: "dbio-auth0-testnet",
-    clientId: "xIEcOQHJ1jJYClYvPS2EFAcinK0w7R2K",
-  },
-};
+export const TORUS_VERIFIER = "dbio-auth0-testnet";
