fbe5457d13cb9df04c67063ebcc4d4c992fc8de7
Stephen Sheldon <20730278+ssheld@users.noreply.github.com>
2022-04-28 12:23:08 -0400

    Remove unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, Fragment } from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { userSelector } from "./store/UserSlice";
 import { initializeIroncoreSDK } from "./actions/UIActions";
