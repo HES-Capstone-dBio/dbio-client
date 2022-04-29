cc62d1d93569b418a2c3de39a6ea976458e342c5
Stephen Sheldon <20730278+ssheld@users.noreply.github.com>
2022-04-29 12:10:42 -0400

    Implement new nav into layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,12 @@
-import MainHeader from "./MainHeader";
 import classes from "./Layout.module.css";
 import { Fragment } from "react";
+import Navigation from "./Navigation";
 
 const Layout = (props) => {
   return (
     <Fragment>
       <div className={classes["snackbar"]} id="snackbar" />
-      <MainHeader />
-      <main>{props.children}</main>
+      <Navigation children={props.children} />
     </Fragment>
   );
 };
