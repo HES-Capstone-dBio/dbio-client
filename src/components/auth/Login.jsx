a8cf08a4aadc894e2969d81473eece7b3bb3b470
Stephen Sheldon <20730278+ssheld@users.noreply.github.com>
2022-04-29 12:09:27 -0400

    Create separate login/logout components

---
+++ src/components/auth/Login.jsx
@@ -0,0 +1,30 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "@mui/material";
+
+const Login = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  const loginButtonHandler = () => {
+    loginWithRedirect();
+  };
+
+  return (
+    <Button
+      onClick={loginButtonHandler}
+      color={"primary"}
+      variant="contained"
+      sx={{
+        m: 3,
+        mt: 6,
+        maxWidth: "12em",
+        maxHeight: "4em",
+        minWidth: "10em",
+        minHeight: "3em",
+      }}
+    >
+      Login/Sign up
+    </Button>
+  );
+};
+
+export default Login;
