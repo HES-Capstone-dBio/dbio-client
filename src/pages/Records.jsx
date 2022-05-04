ed1c880afdf2621e3a5db4637b817bf6fd6bf8a4
Stephen Sheldon <20730278+ssheld@users.noreply.github.com>
2022-05-04 17:58:56 -0400

    Add progress bar for claiming resources

--- src/pages/Records.jsx
+++ src/pages/Records.jsx
@@ -1,13 +1,19 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Grid, Box } from "@mui/material";
 import UnclaimedResourcesTable from "../components/Resources/UnclaimedResourcesTable";
 import ClaimedResourcesTable from "../components/Resources/ClaimedResourcesTable";
-import { listUnclaimedResources } from "../actions/ResourceActions";
-import { listClaimedResources } from "../actions/ResourceActions";
+import {
+  listUnclaimedResources,
+  listClaimedResources,
+} from "../actions/ResourceActions";
+import { resourcesSelector } from "../store/ResourcesSlice";
+import LinearProgress from "@mui/material/LinearProgress";
 
 const Records = () => {
   const dispatch = useDispatch();
+  const { isClaimingResource } = useSelector(resourcesSelector);
+
   // On initial page load get all resources that belong to this user
   useEffect(() => {
     dispatch(listClaimedResources());
@@ -16,10 +22,12 @@ const Records = () => {
 
   return (
     <Box sx={{ flexGrow: 0 }}>
+      {isClaimingResource && <LinearProgress />}
       <Grid container spacing={2}>
         <Grid item xs={12} md={12}>
           <ClaimedResourcesTable />
         </Grid>
+        {isClaimingResource && <LinearProgress />}
         <Grid item xs={12} md={12}>
           <UnclaimedResourcesTable />
         </Grid>
