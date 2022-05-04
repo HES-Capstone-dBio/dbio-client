f3cd0b2554ed60b400dc714fe42561f1460f898a
Stephen Sheldon <20730278+ssheld@users.noreply.github.com>
2022-05-04 17:39:17 -0400

    Create records pages

--- src/pages/Records.jsx
+++ src/pages/Records.jsx
@@ -1,15 +1,30 @@
-import { Fragment } from "react";
-import { Route } from "react-router-dom";
-import RecordDetails from "./RecordDetails";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Grid, Box } from "@mui/material";
+import UnclaimedResourcesTable from "../components/Resources/UnclaimedResourcesTable";
+import ClaimedResourcesTable from "../components/Resources/ClaimedResourcesTable";
+import { listUnclaimedResources } from "../actions/ResourceActions";
+import { listClaimedResources } from "../actions/ResourceActions";
 
 const Records = () => {
+  const dispatch = useDispatch();
+  // On initial page load get all resources that belong to this user
+  useEffect(() => {
+    dispatch(listClaimedResources());
+    dispatch(listUnclaimedResources());
+  }, [dispatch]);
+
   return (
-    <Fragment>
-      <h2>Records pages</h2>
-      <Route path="/records/:recordId">
-        <RecordDetails />
-      </Route>
-    </Fragment>
+    <Box sx={{ flexGrow: 0 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={12}>
+          <ClaimedResourcesTable />
+        </Grid>
+        <Grid item xs={12} md={12}>
+          <UnclaimedResourcesTable />
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
